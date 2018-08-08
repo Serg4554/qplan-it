9ce08fa0718d2c61b1892025dfd8d4d5a06caa4e
Serg <Sergio.357.95@gmail.com>
2018-08-08 13:37:28 +0200

    Renamed to password_recovery

--- common/models/user.js
+++ common/models/user.js
@@ -11,7 +11,7 @@ module.exports = function(model) {
         subject: "Restablecer contraseña de QPlan it!", // Subject line
         text: "Test body", // plain text body
         html: 'Se ha solicitado cambiar la contraseña de tu cuenta QPlan it, puedes introducir una nueva haciendo click ' +
-          '<a href="http://localhost:3000/reset_password?jwt=' + info.accessToken.id + '">aqu&iacute;</a>. Si no ha ' +
+          '<a href="http://localhost:3000/password_recovery?jwt=' + info.accessToken.id + '">aqu&iacute;</a>. Si no ha ' +
           'solicitado cambiar la contraseña, por favor, ignore este correo.<br/>' +
           'El enlace expirará en 15 minutos.'
       });
