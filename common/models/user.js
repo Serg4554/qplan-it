0512892b4f9fc67eed9f3cabc0f30cddd23dc60e
Serg <Sergio.357.95@gmail.com>
2018-08-10 01:03:04 +0200

    Send verification email when user signs up

--- common/models/user.js
+++ common/models/user.js
@@ -3,6 +3,8 @@
 const validator = require('validator');
 const passwordValidator = require('password-validator');
 const passwordSchema = new passwordValidator();
+const TokenGenerator = require('uuid-token-generator');
+const token = new TokenGenerator(256, TokenGenerator.BASE62);
 
 passwordSchema
   .is().min(8)
@@ -16,7 +18,43 @@ module.exports = function(model) {
       return next(new Error("Password must have at least 8 characters, uppercase and lowercase letters"));
     }
 
+    if(!validator.isEmail(ctx.req.body.email)) {
+      return next(new Error("Email is not valid"));
+    }
+
     next();
+  });
+
+  model.observe('before save', async function(ctx) {
+    if(ctx.isNewInstance) {
+      model.findOne({where: {email: ctx.instance.email}})
+        .then(user => {
+          if(!user) {
+            ctx.instance.verificationToken = token.generate();
+          }
+        });
+    }
+  });
+
+  model.observe('after save', async function (ctx) {
+    if(ctx.isNewInstance) {
+      const redirectUrl = encodeURIComponent("http://localhost:3000/account_verified");
+
+      model.findOne({where: {email: ctx.instance.email}})
+        .then(user => {
+          if(user) {
+            const url  = "http://localhost:3001/api/users/confirm?uid=" + user.id + "&token=" + user.verificationToken + "&redirect=" + redirectUrl;
+
+            model.app.models.Email.send({
+              from: "QPlan it! <no-reply@qplan.it>", // sender address
+              to: user.email, // list of receivers
+              subject: "Activa tu cuenta de QPlan it!", // Subject line
+              text: "", // plain text body
+              html: 'Hola ' + ctx.instance.name + ', haz click <a href="' + url + '">aqu&iacute;</a> para activar tu cuenta de QPlan it!'
+            });
+          }
+        });
+    }
   });
 
   model.on('resetPasswordRequest', (info) => {
@@ -25,7 +63,7 @@ module.exports = function(model) {
         from: "QPlan it! <no-reply@qplan.it>", // sender address
         to: info.email, // list of receivers
         subject: "Restablecer contraseña de QPlan it!", // Subject line
-        text: "Test body", // plain text body
+        text: "", // plain text body
         html: 'Se ha solicitado cambiar la contraseña de tu cuenta QPlan it, puedes introducir una nueva haciendo click ' +
           '<a href="http://localhost:3000/password_recovery?jwt=' + info.accessToken.id + '">aqu&iacute;</a>. Si no ha ' +
           'solicitado cambiar la contraseña, por favor, ignore este correo.<br/>' +
