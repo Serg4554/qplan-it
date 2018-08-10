1db68ad35fcce7b5c423eea25e283bf675526b6d
Serg <Sergio.357.95@gmail.com>
2018-08-10 14:56:24 +0200

    NODE_ENV variable set

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
 'use strict';
 
-var loopback = require('loopback');
-var boot = require('loopback-boot');
+const loopback = require('loopback');
+const boot = require('loopback-boot');
 
-var app = module.exports = loopback();
+const app = module.exports = loopback();
 
 app.start = function() {
   // start the web server
   return app.listen(function() {
     app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
+    const baseUrl = app.get('url').replace(/\/$/, '');
+    console.log('Web server: %s (%s)', baseUrl, app.get('env'));
     if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+      const explorerPath = app.get('loopback-component-explorer').mountPath;
+      console.log('REST API: %s%s', baseUrl, explorerPath);
     }
   });
 };
