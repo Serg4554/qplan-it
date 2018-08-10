ed79eedb858b4c02cf53f3ce351f2b8df5539f9c
Serg <Sergio.357.95@gmail.com>
2018-08-10 15:40:20 +0200

    Spaced file

--- server/server.js
+++ server/server.js
@@ -6,11 +6,12 @@ const boot = require('loopback-boot');
 const app = module.exports = loopback();
 
 app.start = function() {
-  // start the web server
-  return app.listen(function() {
+  return app.listen(() => {
     app.emit('started');
+
     const baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server: %s (%s)', baseUrl, app.get('env'));
+
     if (app.get('loopback-component-explorer')) {
       const explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('REST API: %s%s', baseUrl, explorerPath);
@@ -18,8 +19,7 @@ app.start = function() {
   });
 };
 
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
+
 boot(app, __dirname, function(err) {
   if (err) throw err;
 
