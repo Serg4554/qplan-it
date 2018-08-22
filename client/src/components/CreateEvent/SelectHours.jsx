e6dae0aa4d88456ebffb31cb9ed4b67cc7fb9a49
Serg <Sergio.357.95@gmail.com>
2018-08-22 10:19:36 +0200

    Styling improvements

--- client/src/components/CreateEvent/SelectHours.jsx
+++ client/src/components/CreateEvent/SelectHours.jsx
@@ -17,33 +17,35 @@ const SelectHours = (props) => {
       <h3><Translate value="event.selectHoursDescription" /></h3>
 
       <div className="selectHoursContent">
+        <div className="calendar">
+          <div style={{display: "inline-block", width: "355px", textAlign: "center"}}>
+            <Calendar
+              selectedDays={props.selectedDays}
+              allowedDays={props.allowedDays}
+              onSelectedDaysUpdated={props.onSelectedDaysUpdated.bind(this)}
+              appendDays={false}
+              selectRange={true}
+              primaryColor={theme.palette.primary.main}
+              secondaryColor={theme.palette.secondary.light}
+              allowedDaysColor={theme.palette.secondary.clear}
+            />
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={() => props.onSelectedDaysUpdated(props.allowedDays)}
+              disabled={props.selectedDays.length === props.allowedDays.length}
+              style={{marginTop: "4px"}}
+            >
+              <Translate value="common.selectAll" />
+            </Button>
+          </div>
+        </div>
+
         <div className="timeRangePicker">
           <TimeRangePicker
             times={props.times}
             onTimesUpdated={props.onTimesUpdated.bind(this)}
           />
-        </div>
-
-        <div className="calendar">
-          <Calendar
-            selectedDays={props.selectedDays}
-            allowedDays={props.allowedDays}
-            onSelectedDaysUpdated={props.onSelectedDaysUpdated.bind(this)}
-            appendDays={false}
-            selectRange={true}
-            primaryColor={theme.palette.primary.main}
-            secondaryColor={theme.palette.secondary.light}
-            allowedDaysColor={theme.palette.secondary.clear}
-          />
-          <Button
-            color="primary"
-            variant="contained"
-            onClick={() => props.onSelectedDaysUpdated(props.allowedDays)}
-            disabled={props.selectedDays.length === props.allowedDays.length}
-            style={{display: "inline-block", marginTop: "4px"}}
-          >
-            <Translate value="common.selectAll" />
-          </Button>
         </div>
       </div>
     </div>
