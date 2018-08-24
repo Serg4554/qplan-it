30f10d78d2abfd431171b641edaacb1996008639
Serg <Sergio.357.95@gmail.com>
2018-08-24 13:59:42 +0200

    New model for days to allow periods that take 2 days

--- common/models/event.js
+++ common/models/event.js
@@ -4,6 +4,93 @@ let shortid = require('shortid');
 let hashids = require('hashids');
 let bcrypt = require('bcrypt');
 let randtoken = require('rand-token');
+let moment = require('moment');
+
+function ensureValidDays(days) {
+  let startDate, endDate, startBlockedDate, minutesToEnd;
+
+  days.forEach(day => {
+    if(!day.period || !day.period.start || isNaN(day.period.start.getTime())) return;
+
+    startDate = moment(day.period.start);
+    if(!day.period.duration || day.period.duration <= 0) {
+      day.period.duration = Math.floor(moment(day.period.start).endOf('day').diff(startDate) / 60000);
+    } else if(day.period.duration > 1439) {
+      day.period.duration = 1439;
+    }
+    endDate = moment(day.period.start).add(day.period.duration, 'm');
+
+    if(day.blockedPeriods) {
+      day.blockedPeriods.forEach(blockedPeriod => {
+        if(!blockedPeriod.start || isNaN(blockedPeriod.start.getTime())) return;
+
+        startBlockedDate = moment(day.period.start)
+          .hours(blockedPeriod.start.getHours())
+          .minutes(blockedPeriod.start.getMinutes());
+        minutesToEnd = Math.floor(endDate.diff(startBlockedDate) / 60000) % 1440;
+        if(!blockedPeriod.duration || blockedPeriod.duration <= 0) {
+          blockedPeriod.duration = minutesToEnd;
+        } else if(blockedPeriod.duration > minutesToEnd) {
+          blockedPeriod.duration = minutesToEnd;
+        }
+      });
+    }
+  });
+}
+
+function handleCreate(ctx) {
+  if(ctx.instance.days && ctx.instance.days.length !== 0) {
+    ensureValidDays(ctx.instance.days);
+  } else {
+    return;
+  }
+
+  let userId = null;
+  if(ctx.options.accessToken) {
+    userId = ctx.options.accessToken.userId;
+  }
+
+  ctx.instance.id = new hashids(shortid.generate(), 6).encode(1);
+  ctx.instance.ownerId = userId;
+  ctx.instance.claimToken = userId ? null : randtoken.generate(64);
+  if(ctx.instance.password) {
+    ctx.instance.password = bcrypt.hashSync(ctx.instance.password, bcrypt.genSaltSync());
+  }
+}
+
+function handleUpdate(ctx) {
+  return ctx.Model.findById(ctx.instance.id)
+    .then(event => {
+      if(ctx.instance.days && ctx.instance.days.length !== 0) {
+        ensureValidDays(ctx.instance.days);
+      } else {
+        return;
+      }
+
+      ctx.instance.id = event.id;
+      ctx.instance.ownerId = event.ownerId;
+      ctx.instance.claimToken = event.claimToken;
+      if(ctx.instance.password) {
+        ctx.instance.password = bcrypt.hashSync(ctx.instance.password, bcrypt.genSaltSync());
+      }
+    });
+}
+
+function handlePatch(ctx) {
+  return ctx.Model.findById(ctx.currentInstance.id)
+    .then(event => {
+      if(ctx.data.days && ctx.data.days.length !== 0) {
+        ensureValidDays(ctx.data.days);
+      }
+
+      ctx.data.id = event.id;
+      ctx.data.ownerId = event.ownerId;
+      ctx.data.claimToken = event.claimToken;
+      if(ctx.data.password) {
+        ctx.data.password = bcrypt.hashSync(ctx.data.password, bcrypt.genSaltSync());
+      }
+    });
+}
 
 module.exports = function(model) {
   model.validatesPresenceOf("days");
@@ -11,37 +98,11 @@ module.exports = function(model) {
   // Apply checks and modifications before save
   model.observe('before save', async function(ctx) {
     if(ctx.isNewInstance) {
-      let userId = null;
-      if(ctx.options.accessToken) {
-        userId = ctx.options.accessToken.userId;
-      }
-
-      ctx.instance.id = new hashids(shortid.generate(), 6).encode(1);
-      ctx.instance.ownerId = userId;
-      ctx.instance.claimToken = userId ? null : randtoken.generate(64);
-      if(ctx.instance.password) {
-        ctx.instance.password = bcrypt.hashSync(ctx.instance.password, bcrypt.genSaltSync());
-      }
+      await handleCreate(ctx);
     } else if(ctx.instance) {
-      return ctx.Model.findById(ctx.instance.id)
-        .then(event => {
-          ctx.instance.id = event.id;
-          ctx.instance.ownerId = event.ownerId;
-          ctx.instance.claimToken = event.claimToken;
-          if(ctx.instance.password) {
-            ctx.instance.password = bcrypt.hashSync(ctx.instance.password, bcrypt.genSaltSync());
-          }
-        });
+      await handleUpdate(ctx);
     } else if(ctx.currentInstance) {
-      return ctx.Model.findById(ctx.currentInstance.id)
-        .then(event => {
-          ctx.instance.id = event.id;
-          ctx.data.ownerId = event.ownerId;
-          ctx.data.claimToken = event.claimToken;
-          if(ctx.data.password) {
-            ctx.data.password = bcrypt.hashSync(ctx.data.password, bcrypt.genSaltSync());
-          }
-        });
+      await handlePatch(ctx);
     }
   });
 
--- common/models/period.js
+++ common/models/period.js
@@ -1,31 +1,5 @@
 'use strict';
 
 module.exports = function(model) {
-  model.validate('startHour', validateStartHour, {message: 'Invalid hour (from 0 to 23)'});
-  function validateStartHour(err) {
-    if(this.startHour < 0 || this.startHour > 23) {
-      err();
-    }
-  }
 
-  model.validate('startMinute', validateStartMinute, {message: 'Invalid minute (from 0 to 59)'});
-  function validateStartMinute(err) {
-    if(this.startMinute < 0 || this.startMinute > 59) {
-      err();
-    }
-  }
-
-  model.validate('endHour', validateEndHour, {message: 'Invalid hour (from 0 to 23)'});
-  function validateEndHour(err) {
-    if(this.endHour < 0 || this.endHour > 23) {
-      err();
-    }
-  }
-
-  model.validate('endMinute', validateEndMinute, {message: 'Invalid minute (from 0 to 59)'});
-  function validateEndMinute(err) {
-    if(this.endMinute < 0 || this.endMinute > 59) {
-      err();
-    }
-  }
 };
